4cee663f2ae1815df4c1b8ee716f0fcccaf69f69
ayutt <ayoubennaoui20@gmail.com>
2025-06-12 14:24:45 +0100

    working version

--- frontend/betting-analysis/src/app/market-value.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MarketValuePipe } from './market-value.pipe';
-
-describe('MarketValuePipe', () => {
-  it('create an instance', () => {
-    const pipe = new MarketValuePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
